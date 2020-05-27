bf6049f3bae0dfc5dd903d4142211d2728d0fc6d
Robert Yokota <rayokota@gmail.com>
2020-05-27 12:22:47 -0700

    Upgrade to Calcite 1.23.0

--- kareldb-core/src/main/java/org/apache/calcite/jdbc/CalciteMetaImplShim.java
+++ kareldb-core/src/main/java/org/apache/calcite/jdbc/CalciteMetaImplShim.java
@@ -16,81 +16,10 @@
  */
 package org.apache.calcite.jdbc;
 
-import com.google.common.collect.ImmutableList;
 import org.apache.calcite.avatica.AvaticaConnection;
-import org.apache.calcite.avatica.Meta;
-import org.apache.calcite.avatica.NoSuchStatementException;
-import org.apache.calcite.jdbc.CalcitePrepare.Context;
-import org.apache.calcite.runtime.Hook;
-import org.apache.calcite.server.CalciteServerStatement;
-import org.apache.calcite.sql.parser.SqlParser;
-import org.apache.calcite.tools.FrameworkConfig;
-import org.apache.calcite.tools.Frameworks;
-import org.apache.calcite.util.Holder;
-import org.apache.calcite.util.Pair;
-
-import java.sql.SQLException;
 
 public class CalciteMetaImplShim extends CalciteMetaImpl {
     public CalciteMetaImplShim(AvaticaConnection connection) {
         super((CalciteConnectionImpl) connection);
-    }
-
-    // Temporary fix for CALCITE-3324
-    // See https://issues.apache.org/jira/browse/CALCITE-3324
-    @Override
-    public ExecuteResult prepareAndExecute(StatementHandle h,
-                                           String sql, long maxRowCount, int maxRowsInFirstFrame,
-                                           PrepareCallback callback) throws NoSuchStatementException {
-        final CalcitePrepare.CalciteSignature<Object> signature;
-        try {
-            final int updateCount;
-            synchronized (callback.getMonitor()) {
-                callback.clear();
-                final CalciteConnectionImpl calciteConnection = getConnection();
-                final CalciteServerStatement statement =
-                    calciteConnection.server.getStatement(h);
-                final Context context = statement.createPrepareContext();
-                final CalcitePrepare.Query<Object> query = toQuery(context, sql);
-                signature = calciteConnection.parseQuery(query, context, maxRowCount);
-                statement.setSignature(signature);
-                switch (signature.statementType) {
-                    case CREATE:
-                    case DROP:
-                    case ALTER:
-                    case OTHER_DDL:
-                        updateCount = 0; // DDL produces no result set
-                        break;
-                    default:
-                        updateCount = -1; // SELECT and DML produces result set
-                        break;
-                }
-                callback.assign(signature, null, updateCount);
-            }
-            callback.execute();
-            final MetaResultSet metaResultSet =
-                MetaResultSet.create(h.connectionId, h.id, false, signature, null, updateCount);
-            return new ExecuteResult(ImmutableList.of(metaResultSet));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        // TODO: share code with prepare and createIterable
-    }
-
-    /**
-     * Wraps the SQL string in a
-     * {@link org.apache.calcite.jdbc.CalcitePrepare.Query} object, giving the
-     * {@link Hook#STRING_TO_QUERY} hook chance to override.
-     */
-    private CalcitePrepare.Query<Object> toQuery(
-        Context context, String sql) {
-        final Holder<CalcitePrepare.Query<Object>> queryHolder =
-            Holder.of(CalcitePrepare.Query.of(sql));
-        final FrameworkConfig config = Frameworks.newConfigBuilder()
-            .parserConfig(SqlParser.Config.DEFAULT)
-            .defaultSchema(context.getRootSchema().plus())
-            .build();
-        Hook.STRING_TO_QUERY.run(Pair.of(config, queryHolder));
-        return queryHolder.get();
     }
 }
