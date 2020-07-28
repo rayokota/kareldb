b344b77bca95e201ba87723db10dc8900296fa3a
Robert Yokota <rayokota@gmail.com>
2020-07-27 22:44:21 -0700

    Minor reformatting

--- kareldb-core/src/main/java/org/apache/calcite/sql/ddl/ExtensionDdlExecutor.java
+++ kareldb-core/src/main/java/org/apache/calcite/sql/ddl/ExtensionDdlExecutor.java
@@ -73,154 +73,160 @@ import java.util.Objects;
 
 import static org.apache.calcite.util.Static.RESOURCE;
 
-/** Executes the few DDL commands supported by
- * {@link ExtensionSqlParserImpl}. */
+/**
+ * Executes the few DDL commands supported by
+ * {@link ExtensionSqlParserImpl}.
+ */
 public class ExtensionDdlExecutor extends DdlExecutorImpl {
-  static final ExtensionDdlExecutor INSTANCE = new ExtensionDdlExecutor();
+    static final ExtensionDdlExecutor INSTANCE = new ExtensionDdlExecutor();
 
-  /** Parser factory. */
-  @SuppressWarnings("unused") // used via reflection
-  public static final SqlParserImplFactory PARSER_FACTORY =
-      new SqlParserImplFactory() {
-        @Override
-        public SqlAbstractParserImpl getParser(Reader stream) {
-          return ExtensionSqlParserImpl.FACTORY.getParser(stream);
+    /**
+     * Parser factory.
+     */
+    @SuppressWarnings("unused") // used via reflection
+    public static final SqlParserImplFactory PARSER_FACTORY =
+        new SqlParserImplFactory() {
+            @Override
+            public SqlAbstractParserImpl getParser(Reader stream) {
+                return ExtensionSqlParserImpl.FACTORY.getParser(stream);
+            }
+
+            @Override
+            public DdlExecutor getDdlExecutor() {
+                return ExtensionDdlExecutor.INSTANCE;
+            }
+        };
+
+    /**
+     * Executes a {@code CREATE TABLE} command. Called via reflection.
+     */
+    public void execute(SqlCreateTableExtension create, CalcitePrepare.Context context) {
+        final CalciteSchema schema =
+            Schemas.subSchema(context.getRootSchema(),
+                context.getDefaultSchemaPath());
+        final JavaTypeFactory typeFactory = context.getTypeFactory();
+        final RelDataType queryRowType;
+        if (create.query != null) {
+            // A bit of a hack: pretend it's a view, to get its row type
+            final String sql =
+                create.query.toSqlString(CalciteSqlDialect.DEFAULT).getSql();
+            final ViewTableMacro viewTableMacro =
+                ViewTable.viewMacro(schema.plus(), sql, schema.path(null),
+                    context.getObjectPath(), false);
+            final TranslatableTable x = viewTableMacro.apply(ImmutableList.of());
+            queryRowType = x.getRowType(typeFactory);
+
+            if (create.columnList != null
+                && queryRowType.getFieldCount() != create.columnList.size()) {
+                throw SqlUtil.newContextException(create.columnList.getParserPosition(),
+                    RESOURCE.columnCountMismatch());
+            }
+        } else {
+            queryRowType = null;
         }
-
-        @Override
-        public DdlExecutor getDdlExecutor() {
-          return ExtensionDdlExecutor.INSTANCE;
+        final List<SqlNode> columnList;
+        if (create.columnList != null) {
+            columnList = create.columnList.getList();
+        } else {
+            if (queryRowType == null) {
+                // "CREATE TABLE t" is invalid; because there is no "AS query" we need
+                // a list of column names and types, "CREATE TABLE t (INT c)".
+                throw SqlUtil.newContextException(create.name.getParserPosition(),
+                    RESOURCE.createTableRequiresColumnList());
+            }
+            columnList = new ArrayList<>();
+            for (String name : queryRowType.getFieldNames()) {
+                columnList.add(new SqlIdentifier(name, SqlParserPos.ZERO));
+            }
         }
-      };
+        final ImmutableList.Builder<ColumnDef> b = ImmutableList.builder();
+        final RelDataTypeFactory.Builder builder = typeFactory.builder();
+        final RelDataTypeFactory.Builder storedBuilder = typeFactory.builder();
+        final SqlValidator validator = new ContextSqlValidator(context, false);
+        final List<String> keyFields = new ArrayList<>();
+        final List<io.kareldb.schema.ColumnStrategy> strategies = new ArrayList<>();
+        for (Ord<SqlNode> c : Ord.zip(columnList)) {
+            if (c.e instanceof SqlColumnDeclaration) {
+                final SqlColumnDeclaration d = (SqlColumnDeclaration) c.e;
+                final RelDataType type = d.dataType.deriveType(validator, true);
+                builder.add(d.name.getSimple(), type);
+                if (d.strategy != ColumnStrategy.VIRTUAL) {
+                    storedBuilder.add(d.name.getSimple(), type);
+                }
+                b.add(ColumnDef.of(d.expression, type, d.strategy));
+                addStrategy(strategies, d.strategy, d.expression);
+            } else if (c.e instanceof SqlIdentifier) {
+                final SqlIdentifier id = (SqlIdentifier) c.e;
+                if (queryRowType == null) {
+                    throw SqlUtil.newContextException(id.getParserPosition(),
+                        RESOURCE.createTableRequiresColumnTypes(id.getSimple()));
+                }
+                final RelDataTypeField f = queryRowType.getFieldList().get(c.i);
+                final ColumnStrategy strategy = f.getType().isNullable()
+                    ? ColumnStrategy.NULLABLE
+                    : ColumnStrategy.NOT_NULLABLE;
+                b.add(ColumnDef.of(c.e, f.getType(), strategy));
+                builder.add(id.getSimple(), f.getType());
+                storedBuilder.add(id.getSimple(), f.getType());
+                addStrategy(strategies, strategy, null);
+            } else if (c.e instanceof SqlKeyConstraint) {
+                final SqlKeyConstraint keyConstraint = (SqlKeyConstraint) c.e;
+                if (keyConstraint.getOperator() == SqlKeyConstraint.PRIMARY) {
+                    List<SqlNode> operands = keyConstraint.getOperandList();
+                    SqlNodeList sqlNodeList = (SqlNodeList) operands.get(1);
+                    List<SqlNode> keyNodes = sqlNodeList.getList();
+                    for (SqlNode keyNode : keyNodes) {
+                        keyFields.add(((SqlIdentifier) keyNode).getSimple());
+                    }
+                }
+            } else {
+                throw new AssertionError(c.e.getClass());
+            }
+        }
+        final RelDataType rowType = builder.build();
+        final RelDataType storedRowType = storedBuilder.build();
+        final List<ColumnDef> columns = b.build();
+        final InitializerExpressionFactory ief =
+            new NullInitializerExpressionFactory() {
+                @Override
+                public ColumnStrategy generationStrategy(RelOptTable table,
+                                                         int iColumn) {
+                    return columns.get(iColumn).strategy;
+                }
 
-  /** Executes a {@code CREATE TABLE} command. Called via reflection. */
-  public void execute(SqlCreateTableExtension create, CalcitePrepare.Context context) {
-      final CalciteSchema schema =
-          Schemas.subSchema(context.getRootSchema(),
-              context.getDefaultSchemaPath());
-      final JavaTypeFactory typeFactory = context.getTypeFactory();
-      final RelDataType queryRowType;
-      if (create.query != null) {
-          // A bit of a hack: pretend it's a view, to get its row type
-          final String sql =
-              create.query.toSqlString(CalciteSqlDialect.DEFAULT).getSql();
-          final ViewTableMacro viewTableMacro =
-              ViewTable.viewMacro(schema.plus(), sql, schema.path(null),
-                  context.getObjectPath(), false);
-          final TranslatableTable x = viewTableMacro.apply(ImmutableList.of());
-          queryRowType = x.getRowType(typeFactory);
+                @Override
+                public RexNode newColumnDefaultValue(RelOptTable table,
+                                                     int iColumn, InitializerContext context) {
+                    final ColumnDef c = columns.get(iColumn);
+                    if (c.expr != null) {
+                        return context.convertExpression(c.expr);
+                    }
+                    return super.newColumnDefaultValue(table, iColumn, context);
+                }
+            };
 
-          if (create.columnList != null
-              && queryRowType.getFieldCount() != create.columnList.size()) {
-              throw SqlUtil.newContextException(create.columnList.getParserPosition(),
-                  RESOURCE.columnCountMismatch());
-          }
-      } else {
-          queryRowType = null;
-      }
-      final List<SqlNode> columnList;
-      if (create.columnList != null) {
-          columnList = create.columnList.getList();
-      } else {
-          if (queryRowType == null) {
-              // "CREATE TABLE t" is invalid; because there is no "AS query" we need
-              // a list of column names and types, "CREATE TABLE t (INT c)".
-              throw SqlUtil.newContextException(create.name.getParserPosition(),
-                  RESOURCE.createTableRequiresColumnList());
-          }
-          columnList = new ArrayList<>();
-          for (String name : queryRowType.getFieldNames()) {
-              columnList.add(new SqlIdentifier(name, SqlParserPos.ZERO));
-          }
-      }
-      final ImmutableList.Builder<ColumnDef> b = ImmutableList.builder();
-      final RelDataTypeFactory.Builder builder = typeFactory.builder();
-      final RelDataTypeFactory.Builder storedBuilder = typeFactory.builder();
-      final SqlValidator validator = new ContextSqlValidator(context, false);
-      final List<String> keyFields = new ArrayList<>();
-      final List<io.kareldb.schema.ColumnStrategy> strategies = new ArrayList<>();
-      for (Ord<SqlNode> c : Ord.zip(columnList)) {
-          if (c.e instanceof SqlColumnDeclaration) {
-              final SqlColumnDeclaration d = (SqlColumnDeclaration) c.e;
-              final RelDataType type = d.dataType.deriveType(validator, true);
-              builder.add(d.name.getSimple(), type);
-              if (d.strategy != ColumnStrategy.VIRTUAL) {
-                  storedBuilder.add(d.name.getSimple(), type);
-              }
-              b.add(ColumnDef.of(d.expression, type, d.strategy));
-              addStrategy(strategies, d.strategy, d.expression);
-          } else if (c.e instanceof SqlIdentifier) {
-              final SqlIdentifier id = (SqlIdentifier) c.e;
-              if (queryRowType == null) {
-                  throw SqlUtil.newContextException(id.getParserPosition(),
-                      RESOURCE.createTableRequiresColumnTypes(id.getSimple()));
-              }
-              final RelDataTypeField f = queryRowType.getFieldList().get(c.i);
-              final ColumnStrategy strategy = f.getType().isNullable()
-                  ? ColumnStrategy.NULLABLE
-                  : ColumnStrategy.NOT_NULLABLE;
-              b.add(ColumnDef.of(c.e, f.getType(), strategy));
-              builder.add(id.getSimple(), f.getType());
-              storedBuilder.add(id.getSimple(), f.getType());
-              addStrategy(strategies, strategy, null);
-          } else if (c.e instanceof SqlKeyConstraint) {
-              final SqlKeyConstraint keyConstraint = (SqlKeyConstraint) c.e;
-              if (keyConstraint.getOperator() == SqlKeyConstraint.PRIMARY) {
-                  List<SqlNode> operands = keyConstraint.getOperandList();
-                  SqlNodeList sqlNodeList = (SqlNodeList) operands.get(1);
-                  List<SqlNode> keyNodes = sqlNodeList.getList();
-                  for (SqlNode keyNode : keyNodes) {
-                      keyFields.add(((SqlIdentifier) keyNode).getSimple());
-                  }
-              }
-          } else {
-              throw new AssertionError(c.e.getClass());
-          }
-      }
-      final RelDataType rowType = builder.build();
-      final RelDataType storedRowType = storedBuilder.build();
-      final List<ColumnDef> columns = b.build();
-      final InitializerExpressionFactory ief =
-          new NullInitializerExpressionFactory() {
-              @Override
-              public ColumnStrategy generationStrategy(RelOptTable table,
-                                                       int iColumn) {
-                  return columns.get(iColumn).strategy;
-              }
-
-              @Override
-              public RexNode newColumnDefaultValue(RelOptTable table,
-                                                   int iColumn, InitializerContext context) {
-                  final ColumnDef c = columns.get(iColumn);
-                  if (c.expr != null) {
-                      return context.convertExpression(c.expr);
-                  }
-                  return super.newColumnDefaultValue(table, iColumn, context);
-              }
-          };
-
-      Schema schemaPlus = schema.plus().unwrap(Schema.class);
-      String tableName = create.name.getSimple();
-      if (schemaPlus.getTable(tableName) != null) {
-          // Table exists.
-          if (!create.ifNotExists) {
-              // They did not specify IF NOT EXISTS, so give error.
-              throw SqlUtil.newContextException(create.name.getParserPosition(),
-                  RESOURCE.tableExists(tableName));
-          }
-          return;
-      }
-      if (keyFields.isEmpty()) {
-          // Use first column as primary key
-          keyFields.add(rowType.getFieldList().get(0).getName());
-      }
-      // Table does not exist. Create it.
-      Table table = schemaPlus.createTable(tableName, null, new RelDef(rowType, keyFields, strategies));
-      schema.add(tableName, table);
-      if (create.query != null) {
-          populate(create.name, create.query, context);
-      }
-  }
+        Schema schemaPlus = schema.plus().unwrap(Schema.class);
+        String tableName = create.name.getSimple();
+        if (schemaPlus.getTable(tableName) != null) {
+            // Table exists.
+            if (!create.ifNotExists) {
+                // They did not specify IF NOT EXISTS, so give error.
+                throw SqlUtil.newContextException(create.name.getParserPosition(),
+                    RESOURCE.tableExists(tableName));
+            }
+            return;
+        }
+        if (keyFields.isEmpty()) {
+            // Use first column as primary key
+            keyFields.add(rowType.getFieldList().get(0).getName());
+        }
+        // Table does not exist. Create it.
+        Table table = schemaPlus.createTable(tableName, null, new RelDef(rowType, keyFields, strategies));
+        schema.add(tableName, table);
+        if (create.query != null) {
+            populate(create.name, create.query, context);
+        }
+    }
 
     public void execute(SqlAlterTableExtension alter, CalcitePrepare.Context context) {
         final CalciteSchema schema =
@@ -355,42 +361,45 @@ public class ExtensionDdlExecutor extends DdlExecutorImpl {
             return new ColumnDef(expr, type, strategy);
         }
     }
-  /** Populates the table called {@code name} by executing {@code query}. */
-  protected static void populate(SqlIdentifier name, SqlNode query,
-                                 CalcitePrepare.Context context) {
-    // Generate, prepare and execute an "INSERT INTO table query" statement.
-    // (It's a bit inefficient that we convert from SqlNode to SQL and back
-    // again.)
-    final FrameworkConfig config = Frameworks.newConfigBuilder()
-        .defaultSchema(
-            Objects.requireNonNull(
-                Schemas.subSchema(context.getRootSchema(),
-                    context.getDefaultSchemaPath())).plus())
-        .build();
-    final Planner planner = Frameworks.getPlanner(config);
-    try {
-      final StringBuilder buf = new StringBuilder();
-      final SqlPrettyWriter w =
-          new SqlPrettyWriter(
-              SqlPrettyWriter.config()
-                  .withDialect(CalciteSqlDialect.DEFAULT)
-                  .withAlwaysUseParentheses(false),
-              buf);
-      buf.append("INSERT INTO ");
-      name.unparse(w, 0, 0);
-      buf.append(" ");
-      query.unparse(w, 0, 0);
-      final String sql = buf.toString();
-      final SqlNode query1 = planner.parse(sql);
-      final SqlNode query2 = planner.validate(query1);
-      final RelRoot r = planner.rel(query2);
-      final PreparedStatement prepare = context.getRelRunner().prepare(r.rel);
-      int rowCount = prepare.executeUpdate();
-      Util.discard(rowCount);
-      prepare.close();
-    } catch (SqlParseException | ValidationException
-        | RelConversionException | SQLException e) {
-      throw new RuntimeException(e);
+   
+    /**
+     * Populates the table called {@code name} by executing {@code query}.
+     */
+    protected static void populate(SqlIdentifier name, SqlNode query,
+                                   CalcitePrepare.Context context) {
+        // Generate, prepare and execute an "INSERT INTO table query" statement.
+        // (It's a bit inefficient that we convert from SqlNode to SQL and back
+        // again.)
+        final FrameworkConfig config = Frameworks.newConfigBuilder()
+            .defaultSchema(
+                Objects.requireNonNull(
+                    Schemas.subSchema(context.getRootSchema(),
+                        context.getDefaultSchemaPath())).plus())
+            .build();
+        final Planner planner = Frameworks.getPlanner(config);
+        try {
+            final StringBuilder buf = new StringBuilder();
+            final SqlPrettyWriter w =
+                new SqlPrettyWriter(
+                    SqlPrettyWriter.config()
+                        .withDialect(CalciteSqlDialect.DEFAULT)
+                        .withAlwaysUseParentheses(false),
+                    buf);
+            buf.append("INSERT INTO ");
+            name.unparse(w, 0, 0);
+            buf.append(" ");
+            query.unparse(w, 0, 0);
+            final String sql = buf.toString();
+            final SqlNode query1 = planner.parse(sql);
+            final SqlNode query2 = planner.validate(query1);
+            final RelRoot r = planner.rel(query2);
+            final PreparedStatement prepare = context.getRelRunner().prepare(r.rel);
+            int rowCount = prepare.executeUpdate();
+            Util.discard(rowCount);
+            prepare.close();
+        } catch (SqlParseException | ValidationException
+            | RelConversionException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
 }
